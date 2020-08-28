bdad287ab5914faa90b179665f2268480c5ab482
tg <tamaghan@buffalo.edu>
2020-08-28 00:26:47 -0400

    yeet

--- Final2/sdr.py
+++ Final2/sdr.py
@@ -6,6 +6,7 @@ import time
 class SDR: 
 	def __init__(self):
 		print('initializing SDR...')
+		'''
 		webbrowser.open('http://192.168.4.1:8080/init', new=2)
 		time.sleep(1)
 		mouse.click(920,117)
@@ -16,6 +17,7 @@ class SDR:
 		time.sleep(2)
 		mouse.click(470,445)
 		time.sleep(30)
+		'''
 		print('DONE')
 
 	def check_SDR(self):
@@ -27,10 +29,11 @@ class SDR:
 		# need to collect at least 10 degrees and confirm the avg
 		degree = browser.find_element_by_id('doa').text
 		browser.quit()
-
+		degree = int(degree.split()[2])
 		# potentially change the degree threshold 
 		if degree > 180:
 			return -1
 		else:
 			return 1
+			
 
